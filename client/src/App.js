d2267debbea5700b444ab8277cd4a5f1e543c8e7
toofarm <toofarm@gmail.com>
2021-03-04 11:02:24 -0800

    Update nginx port mapping

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Fibonnaci(sp?) Calculator</h1>
+          <h1>Fibonnaci Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
           <div>
