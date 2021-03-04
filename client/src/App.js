228469af50d8dc84db00f5c0eff6d4279dcb61b3
toofarm <toofarm@gmail.com>
2021-03-04 10:47:15 -0800

    Change h1

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Fibonnaci(sp?) Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
           <div>
@@ -31,5 +32,3 @@ function App() {
 }
 
 export default App;
-
-// docker run -it IMAGE_ID
